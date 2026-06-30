b9c8d6cdc6b820f97fca2e53324f43041aabddbd
Roy Hashimoto <roy@shoestringresearch.com>
2026-06-30 12:09:25 -0700

    Create table and submit transactions.

--- main.js
+++ main.js
@@ -10,6 +10,33 @@ const OUTPUT_TIME_FORMAT = {
   fractionalSecondDigits: 3,
   };
 
+(async () => {
+  try {
+    // Set up database connection in a Worker.
+    const dbWorker = new Worker(
+      'main-worker.js?path=' + encodeURIComponent(DB_PATH),
+      { type: 'module' });
+    const dbExec = Comlink.wrap(dbWorker);
+
+    // Turn off auto checkpoint.
+    await dbExec(`PRAGMA wal_auto_checkpoint=0`);
+
+    // Create a table.
+    await dbExec(`CREATE TABLE IF NOT EXISTS t (key INTEGER PRIMARY KEY, value)`);
+
+    // Submit a lot of write transactions.
+    for (let i = 0; i < 10000; i++) {
+      if (i % 1000 === 0) {
+        log(`Transaction ${i}...`);
+      }
+      await dbExec(`INSERT OR REPLACE INTO t VALUES (${i % 1000}, '${Math.random().toString(16)}')`);
+    }
+    log('Done');
+  } catch (e) {
+    log(e?.stack ?? e?.message ?? e);
+  }
+})();
+
 function log(...args) {
   const timeString = new Date().toLocaleTimeString([], OUTPUT_TIME_FORMAT);
   const message = args.map(a => {
@@ -23,16 +50,3 @@ new BroadcastChannel('log').addEventListener('message', (event) => {
 });
 
 log('Hello, world!');
-(async () => {
-  try {
-    const dbWorker = new Worker(
-      'main-worker.js?path=' + encodeURIComponent(DB_PATH),
-      { type: 'module' });
-    const dbExec = Comlink.wrap(dbWorker);
-    log(await dbExec(`SELECT 1+1`));
-  } catch (e) {
-    log(e?.stack ?? e?.message ?? e);
-  }
-})();
-
-
